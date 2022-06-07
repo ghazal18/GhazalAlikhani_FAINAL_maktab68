2028404fc42497b105b2fd8e2c70a953d70b8f6e
ghazal Alikhani <ghazal.alikhani123@Gmail.com>
2022-06-07 16:42:33 +0430

    add list of showAll

--- app/src/main/java/com/example/myapplication/adaptor/ListOfProductAdaptor.kt
+++ app/src/main/java/com/example/myapplication/adaptor/ListOfProductAdaptor.kt
@@ -1,4 +1,60 @@
 package com.example.myapplication.adaptor
 
-class ListOfProductAdaptor {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.myapplication.R
+import com.example.myapplication.databinding.CategoryListItemViewBinding
+import com.example.myapplication.databinding.ProductListItemViewBinding
+import com.example.myapplication.databinding.ProductShowAllItemViewBinding
+import com.example.myapplication.model.CategoriesItem
+import com.example.myapplication.model.ProductsItem
+
+typealias ProductShowAllClickHandler = (ProductsItem) -> Unit
+class ListOfProductAdaptor(val onClick: ProductShowAllClickHandler)
+    : ListAdapter<ProductsItem, ListOfProductAdaptor.ItemHolder>(ProductDiffCallback) {
+
+    class ItemHolder(val binding: ProductShowAllItemViewBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(product: ProductsItem, onClick: ProductShowAllClickHandler) {
+            Glide.with(itemView).load(product.images[0].src).into(binding.productImageView)
+            binding.linear.setOnClickListener {
+                onClick.invoke(product)
+            }
+        }
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val binding: ProductShowAllItemViewBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.product_show_all_item_view,
+            parent, false
+        )
+        return ItemHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        val product = getItem(position)
+        holder.binding.productt = product
+        holder.bind(product, onClick)
+    }
+
+}
+
+
+object ProductDiffCallback : DiffUtil.ItemCallback<ProductsItem>() {
+
+    override fun areItemsTheSame(oldItem: ProductsItem, newItem: ProductsItem): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: ProductsItem, newItem: ProductsItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
 }
--- app/src/main/java/com/example/myapplication/ui/ShowAllFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/ShowAllFragment.kt
@@ -5,12 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import com.example.myapplication.R
+import com.example.myapplication.adaptor.ListOfProductAdaptor
+import com.example.myapplication.databinding.FragmentShowAllBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class ShowAllFragment : Fragment() {
     val args: ShowAllFragmentArgs by navArgs()
+    val viewModel: MainProductViewModel by viewModels()
+    lateinit var binding: FragmentShowAllBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -21,16 +28,36 @@ class ShowAllFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.fragment_show_all, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_show_all, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         var type = args.listType
-        if (type == "best"){
+        val adaptor = ListOfProductAdaptor() { Product ->
 
-        }else if(type == "new"){
+        }
+        binding.showAllRecyclerView.adapter = adaptor
+        if (type == "best") {
+            viewModel.productRatingList.observe(viewLifecycleOwner) {
+                if (it != null) {
+                    adaptor.submitList(it)
+                }
+            }
+        } else if (type == "new") {
+            viewModel.productDataList.observe(viewLifecycleOwner) {
+                if (it != null) {
+                    adaptor.submitList(it)
+                }
+            }
 
-        }else{
+        } else {
+            viewModel.productPopularityList.observe(viewLifecycleOwner) {
+                if (it != null) {
+                    adaptor.submitList(it)
+                }
+            }
 
         }
     }
