fcaf9aaf643747cf83223749f00e57f892a19b3b
ghazal Alikhani <ghazal.alikhani123@Gmail.com>
2022-06-08 11:20:53 +0430

    change my secret key

--- app/src/main/java/com/example/myapplication/network/ApiService.kt
+++ app/src/main/java/com/example/myapplication/network/ApiService.kt
@@ -6,8 +6,8 @@ import com.example.myapplication.model.ProductsItem
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-const val CONSUMER_KEY = "ck_6b55bb0ff3ea0b7bf4c0aa879af50061964ce38f"
-const val CONSUMER_SECRET = "cs_9b09e5125acffdd27bbe72843ced49db5f8bffb4"
+const val CONSUMER_KEY = "ck_1638823b5544cd50791d0703153dccdf48c82e8c"
+const val CONSUMER_SECRET = "cs_dd772c1e5ac603602694fd19601a830c05c1deb3"
 const val ORDER_BY_RATING = "rating"
 const val ORDER_BY_DATE = "date"
 const val ORDER_BY_POPULARITY = "popularity"
