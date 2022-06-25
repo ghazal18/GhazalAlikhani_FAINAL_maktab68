e061d1ef6b9076218f8e345b811565ea03ad8e22
ghazal Alikhani <ghazal.alikhani123@Gmail.com>
2022-06-25 18:20:59 +0430

    add dark mode

--- app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/ui/activity/MainActivity.kt
@@ -5,6 +5,7 @@ package com.example.myapplication.ui.activity
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.widget.Toolbar
 import androidx.navigation.fragment.NavHostFragment
 import com.example.myapplication.R
@@ -20,7 +21,9 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         val buttonProfile = findViewById<ImageButton>(R.id.buttonProfile)
         val buttonSearch = findViewById<ImageButton>(R.id.buttonSearch)
+        val buttonDarkMode = findViewById<ImageButton>(R.id.buttonDarkMode)
         val buttonShop = findViewById<ImageButton>(R.id.buttonshop)
+        var darkMode = false
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
@@ -33,6 +36,17 @@ class MainActivity : AppCompatActivity() {
         }
         buttonSearch.setOnClickListener {
             navController.navigate(R.id.searchFragment)
+        }
+        buttonDarkMode.setImageResource(R.drawable.ic_baseline_dark_mode_24)
+        buttonDarkMode.setOnClickListener {
+            if(!darkMode){
+                buttonDarkMode.setImageResource(R.drawable.ic_baseline_wb_sunny_24)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                buttonDarkMode.setImageResource(R.drawable.ic_baseline_dark_mode_24)
+            }
+                darkMode = !darkMode
         }
 
     }
--- app/src/main/java/com/example/myapplication/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/fragments/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
