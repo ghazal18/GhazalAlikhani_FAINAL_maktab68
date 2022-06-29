fb9e793cf53e746106a5515209d43b82ab45ca8d
ghazal Alikhani <ghazal.alikhani123@Gmail.com>
2022-06-29 23:34:03 +0430

    add header for recyclerView

--- app/src/main/java/com/example/myapplication/adaptor/ProductAdaptor.kt
+++ app/src/main/java/com/example/myapplication/adaptor/ProductAdaptor.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication.adaptor
 
+import android.renderscript.ScriptGroup
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -10,39 +12,64 @@ import com.example.myapplication.R
 import com.example.myapplication.databinding.ProductListItemViewBinding
 import com.example.myapplication.model.ProductsItem
 import com.bumptech.glide.Glide
+import com.example.myapplication.databinding.HeaderProductBinding
 
 typealias ProductClickHandler = (ProductsItem) -> Unit
 
 class ProductAdaptor(val onClick: ProductClickHandler) :
-    ListAdapter<ProductsItem, ProductAdaptor.ItemHolder>(MovieDiffCallback) {
+    ListAdapter<ProductsItem, Holder<*>>(MovieDiffCallback) {
 
-    class ItemHolder(val binding: ProductListItemViewBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        fun bind(product: ProductsItem, onClick: ProductClickHandler) {
+    class ItemHolder(val binding1: ProductListItemViewBinding) :
+        Holder<HeaderProductBinding>(binding1) {
+
+        override fun bind(product: ProductsItem, onClick: ProductClickHandler) {
+            binding1.textViewProductName.text = product.name
             try {
-                Glide.with(itemView).load(product.images[0].src).into(binding.productImage)
+                Glide.with(itemView).load(product.images[0].src).into(binding1.productImage)
             } catch (e: Exception) {
-                binding.productImage.setImageResource(R.drawable.ic_connection_error)
+                binding1.productImage.setImageResource(R.drawable.ic_connection_error)
             }
-            binding.linear.setOnClickListener {
+            binding1.linear.setOnClickListener {
                 onClick.invoke(product)
             }
         }
     }
 
+    class HeaderHolder(val binding2: HeaderProductBinding) :
+        Holder<HeaderProductBinding>(binding2) {
+        override fun bind(product: ProductsItem, onClick: ProductClickHandler) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
-        val binding: ProductListItemViewBinding = DataBindingUtil.inflate(
-            LayoutInflater.from(parent.context),
-            R.layout.product_list_item_view,
-            parent, false
-        )
-        return ItemHolder(binding)
+        }
     }
 
-    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder<*> {
+        return if (viewType == 0) {
+            val binding: HeaderProductBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.header_product,
+                parent, false
+            )
+            HeaderHolder(binding)
+        } else {
+            val binding: ProductListItemViewBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
+                R.layout.product_list_item_view,
+                parent, false
+            )
+            ItemHolder(binding)
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if(position == 0 ){
+            return 0
+        }else{
+            return 1
+        }
+    }
+
+    override fun onBindViewHolder(holder: Holder<*>, position: Int) {
         val product = getItem(position)
-        holder.binding.product = product
         holder.bind(product, onClick)
     }
 
@@ -59,4 +86,9 @@ object MovieDiffCallback : DiffUtil.ItemCallback<ProductsItem>() {
         return oldItem.id == newItem.id
     }
 
+}
+
+abstract class Holder<T>(val binding: ViewDataBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+    abstract fun bind(product: ProductsItem, onClick: ProductClickHandler)
 }
